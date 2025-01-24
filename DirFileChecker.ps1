89f05bd6a673a42567ede0b1f560281e0fa03b87
Maikel-HA17 <5815957@alu.murciaeduca.es>
2025-01-24 16:36:00 +0100

    Update DirFileChecker.ps1

--- DirFileChecker.ps1
+++ DirFileChecker.ps1
@@ -1,6 +1,26 @@
 # Variable menú
 $option = 0
 
+# Admin o no
+$Admin = [boolean] (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $Admin){
+    $Init = Read-Host "Necesitas ejecutar este script como administrador, ejecutar (s/N)"
+
+    if ($Init -ieq "s"){
+        Start-Process -FilePath "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File", "`"$PSCommandPath`"" -Verb RunAs
+        exit
+    } else {
+        Write-Error "El script debe ser ejecutado como administrador, Saliendo !!"
+    }
+} 
+
+# Actualizar o no
+$Update = Read-Host "Actualizar Windows Defender (s/n)"
+if ($Update -eq 's'){
+    Update-MpSignature
+} 
+
 # Función mostrar menú
 function showmenu{
 Write-Host "1. Escanear fichero"
@@ -14,12 +34,22 @@ showmenu # Llamar al menú
 $option = Read-Host "Introduce una opción"
 
 if ($option -eq '1'){
-    $fichero = Read-Host "Introduce el nombre del fichero: "
-    C:\Program Files\Windows Defender\MpCmdRun.exe -Scan -Scantype 3 -File $fichero
+    $fichero = Read-Host "Introduce el nombre del fichero"
+
+    if (Test-Path $fichero){
+        Start-MpScan -ScanPath $fichero -ScanType CustomScan
+    } else {
+        Write-Error "No se encuentra el fichero"
+    }    
 } 
 elseif ($option -eq '2'){
-    $rute = Read-Host "Introduce la ruta hacia el directorio: "
-    Start-MpScan -ScanPath $rute 
+    $rute = Read-Host "Introduce la ruta hacia el directorio"
+
+    if (Test-Path $rute){
+        Start-MpScan -ScanPath $rute -ScanType CustomScan
+    } else {
+        Write-Error "No se encuentra el directorio"
+    }
 } 
 elseif ($option -eq '3'){
     Write-Host "Saliendoo !!"
@@ -27,7 +57,7 @@ elseif ($option -eq '3'){
 
 else {
     Write-Host "Opción no válida"
-    clear
+    Clear-Host
 }
 
 }
